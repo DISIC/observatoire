60cbf60a423c5ceb32b767fbe68cbe94b94dba3f
slauriere <slauriere@xwiki.com>
2021-02-09 17:46:06 +0100

    #880: Implémentation de l'année glissante pour les résultats du tableau de bord administration pour les avis - DefaultAvisStatsManager: introduce helper method "createAvisStatsQuery"   which computes a query to compute statistics either for one demarche   or for all, either between two dates or for the whole dataset - Add a method which computes the current rolling year interval - By default, compute the statistics for the current year only

--- extensions/tools-api/src/main/java/com/xwiki/projects/dinsic/wikidemarches/extensions/tools/DefaultAvisStatsManager.java
+++ extensions/tools-api/src/main/java/com/xwiki/projects/dinsic/wikidemarches/extensions/tools/DefaultAvisStatsManager.java
@@ -1,5 +1,7 @@
 package com.xwiki.projects.dinsic.wikidemarches.extensions.tools;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -45,6 +47,61 @@ public class DefaultAvisStatsManager implements AvisStatsManager
     @Inject
     private Provider<XWikiContext> contextProvider;
 
+    /**
+     * Returns a date interval which starts one year from now plus one day and ends now.
+     * @return An array of two dates representing the past year.
+     */
+    public Date[] getCurrentRollingYear() {
+        Calendar maxDate = Calendar.getInstance();
+        Calendar minDate = Calendar.getInstance();
+        minDate.add(Calendar.YEAR, -1);
+        minDate.add(Calendar.DATE, 1);
+        return new Date[]{minDate.getTime(), maxDate.getTime()};
+    }
+
+    /**
+     * Creates a query for computing avis statistics either for a single demarche (if the demarche argument is not null),
+     * either for all demarches (if the demarche argument is null), optionally between two dates if the corresponding
+     * dateBounds argument is not null.
+     * @param demarche DocumentReference of a demarche for which the statistics need to be computed, or null if the query should cover all demarches.
+     * @param dateBounds Date interval within with the avis were created, or null if all avis from day 1 should be counted.
+     * @return a Query object that can be executed
+     * @throws QueryException
+     */
+    protected Query createAvisStatsQuery(DocumentReference demarche, Date[] dateBounds) throws QueryException {
+        // The query below will return values for Demarches having at least one Avis.
+        // for now the 'vote' of an avis cannot be null (it's either empty value, or true or false),
+        // and thus we can compute both score average and vote count in the same request.
+        // However, in theory the vote can be null and thus should be computed in a separate query (or outer joined), so
+        // that the join with the vote value does not exclude avis from the counting of avis.
+        String hql = "select demarche.value, count(*), avg(score.value), " +
+                "sum(case when vote.value = 'true' then 1 else 0 end) from XWikiDocument as doc, BaseObject as avis, " +
+                "IntegerProperty as score, StringProperty as vote, StringProperty as demarche " +
+                "where avis.name = doc.fullName and avis.className = :avisClass and score.id.id = avis.id " +
+                "and score.id.name = :scoreProperty and demarche.id.id = avis.id and demarche.id.name = :demarcheProperty " +
+                "and vote.id.id = avis.id and vote.id.name = :voteProperty and score.value > 0";
+        if (demarche != null) {
+            hql += " and demarche.value = :demarche";
+        }
+        if (dateBounds != null) {
+            hql += " and doc.creationDate >= :minDate and doc.creationDate <= :maxDate";
+        }
+        hql += " group by demarche.value order by demarche.value";
+        Query query = this.queryManager.createQuery(hql, Query.HQL);
+        query.bindValue("avisClass", AVIS_CLASS_NAME);
+        query.bindValue("scoreProperty", AVIS_SCORE_PROPERTY_NAME);
+        query.bindValue("voteProperty", AVIS_VOTE_PROPERTY_NAME);
+        query.bindValue("demarcheProperty", AVIS_DEMARCHE_PROPERTY_NAME);
+        if (demarche != null) {
+            query.bindValue("demarche", compactWikiSerializer.serialize(demarche));
+        }
+        if (dateBounds != null) {
+            query.bindValue("minDate", dateBounds[0]);
+            query.bindValue("maxDate", dateBounds[1]);
+        }
+        return query;
+    }
+
     public synchronized void computeAvisStats(DocumentReference demarcheReference, boolean save)
             throws QueryException, XWikiException
     {
@@ -52,68 +109,34 @@ public class DefaultAvisStatsManager implements AvisStatsManager
     }
 
     /**
-     * Computes the AvisStats values and saves the AvisStats object for the demarche passed as parameter. We assume that
-     * the demarcheReference is valid, no check is performed. <br> TODO: is synchronized enough to cover the thread
-     * safety?
+     * Computes the AvisStats values for the current rolling year, and saves the AvisStats object for
+     * the demarche passed as parameter. We assume that the demarcheReference is valid, no check is performed.
+     * <br> TODO: is synchronized enough to cover the thread safety?
      */
     public synchronized void computeAvisStats(XWikiDocument demarche, boolean save)
             throws QueryException, XWikiException
     {
-
-        Query query = this.queryManager
-                .createQuery(
-                        "select count(*), avg(score.value), sum(case when vote.value='true' then 1 else 0 end) from "
-                                + "BaseObject as avis, IntegerProperty as score, StringProperty as vote, "
-                                + "StringProperty as demarche where avis.className = :avisClass "
-                                + "and avis.id = score.id.id and score.id.name = :scoreProperty "
-                                + "and avis.id = vote.id.id and vote.id.name = :voteProperty "
-                                + "and demarche.id.id = avis.id and demarche.id.name = :demarcheProperty and demarche.value = "
-                                + ":demarche and score.value > 0", Query.HQL);
-        query = query.bindValue("demarche", compactWikiSerializer.serialize(demarche.getDocumentReference()));
-        query.bindValue("avisClass", AVIS_CLASS_NAME);
-        query.bindValue("scoreProperty", AVIS_SCORE_PROPERTY_NAME);
-        query.bindValue("voteProperty", AVIS_VOTE_PROPERTY_NAME);
-        query.bindValue("demarcheProperty", AVIS_DEMARCHE_PROPERTY_NAME);
-
+        Query query = createAvisStatsQuery(demarche.getDocumentReference(), getCurrentRollingYear());
         List results = query.execute();
         Object[] result = (Object[]) results.get(0);
-        long occurrences = (long) result[0];
-        double satisfactionIndex = result[1] != null ? (double) result[1] : 0;
-        long votes = result[2] != null ? (long) result[2] : 0;
+        long occurrences = (long) result[1];
+        double satisfactionIndex = result[2] != null ? (double) result[2] : 0;
+        long votes = result[3] != null ? (long) result[3] : 0;
         logger.debug("Computing stats for demarche {} - occurences: {} satisfaction index: {} votes: {}",
                 demarche.getDocumentReference(), occurrences, satisfactionIndex, votes);
         setAvisStatsValues(demarche, occurrences, satisfactionIndex, votes, save, false);
     }
 
     /**
-     * Computes the AvisStats values and saves the AvisStats objects for all demarches having at least one avis. For
-     * those which have no avis, the AvisStats object is in principle initialized upon demarche creation by a
-     * {@link DemarcheEventListener}.
+     * Computes the AvisStats values for the current rolling year and saves the AvisStats objects for all demarches
+     * having at least one avis. For those which have no avis, the AvisStats object is in principle initialized upon
+     * demarche creation by a {@link DemarcheEventListener}.
      */
     public void computeAvisStats() throws QueryException, XWikiException
     {
         logger.debug("Starting to compute stats for all demarches");
-        // The query below will return values for Demarches having at least one Avis.
-        // for now the 'vote' of an avis cannot be null (it's either empty value, or true or false),
-        // and thus we can compute both score average and vote count in the same request.
-        // However, in theory the vote can be null and thus should be computed in a separate query (or outer joined), so
-        // that the join with the vote value does not exclude avis from the counting of avis.
-        Query query = this.queryManager.createQuery("select demarche.value, count(*), avg(score.value), "
-                + "sum(case when vote.value='true' then 1 else 0 end) from BaseObject as avis, "
-                + "IntegerProperty as score, StringProperty as vote, StringProperty as demarche where "
-                + "avis.className = :avisClass "
-                + "and avis.id = score.id.id and score.id.name = :scoreProperty and demarche.id.id = avis.id "
-                + "and avis.id = vote.id.id and vote.id.name = :voteProperty and demarche.id.id = avis.id and "
-                + "demarche.id.name = :demarcheProperty and score.value > 0 group by demarche.value order "
-                + "by demarche.value", Query.HQL);
-
-        query.bindValue("avisClass", AVIS_CLASS_NAME);
-        query.bindValue("scoreProperty", AVIS_SCORE_PROPERTY_NAME);
-        query.bindValue("voteProperty", AVIS_VOTE_PROPERTY_NAME);
-        query.bindValue("demarcheProperty", AVIS_DEMARCHE_PROPERTY_NAME);
-
+        Query query = createAvisStatsQuery(null, getCurrentRollingYear());
         int handledDemarches = 0;
-
         List results = query.execute();
         for (Object result : results) {
             Object[] values = (Object[]) result;
