4a8a114ddd0288278996ae9c291da5b5432abab0
= <=>
2022-06-11 12:24:42 +0200

    Agrégation des données des avis pour remplacer tous les usages qui calculent aujourd'hui ces données à partir des avis détaillés #1129

--- aggregates/calcupdate.sql
+++ aggregates/calcupdate.sql
@@ -20,14 +20,14 @@ agg.diff_suite = data.diff_suite,
 agg.diff_autre = data.diff_autre;
 update avis_byday agg JOIN (
 select cast(xwd_creation_date as date) as day, xws_value as demarche,
-sum(CASE WHEN diff.xwl_value='proche' THEN 1 ELSE 0 END) as aide_proche,
-sum(CASE WHEN diff.xwl_value='association' THEN 1 ELSE 0 END) as aide_association,
-sum(CASE WHEN diff.xwl_value='agent' THEN 1 ELSE 0 END) as aide_agent,
-sum(CASE WHEN diff.xwl_value='internet' THEN 1 ELSE 0 END) as aide_internet,
-sum(CASE WHEN diff.xwl_value='autre' THEN 1 ELSE 0 END) as aide_autre,
-sum(CASE WHEN diff.xwl_value='aucune' THEN 1 ELSE 0 END) as aide_aucune
+sum(CASE WHEN aide.xwl_value='proche' THEN 1 ELSE 0 END) as aide_proche,
+sum(CASE WHEN aide.xwl_value='association' THEN 1 ELSE 0 END) as aide_association,
+sum(CASE WHEN aide.xwl_value='agent' THEN 1 ELSE 0 END) as aide_agent,
+sum(CASE WHEN aide.xwl_value='internet' THEN 1 ELSE 0 END) as aide_internet,
+sum(CASE WHEN aide.xwl_value='autre' THEN 1 ELSE 0 END) as aide_autre,
+sum(CASE WHEN aide.xwl_value='aucune' THEN 1 ELSE 0 END) as aide_aucune
 from xwikidoc use index (doc_creation_date), xwikistrings demarche,
-xwikiobjects avis left outer join xwikilistitems diff on (xwo_id=xwl_id and xwl_name='aide')
+xwikiobjects avis left outer join xwikilistitems aide on (xwo_id=xwl_id and xwl_name='aide')
 where xwd_fullname=xwo_name and xwo_classname='Avis.Code.AvisClass'
 and xwo_id=xws_id and xws_name='demarche'
 and date_format(xwd_creation_date,'%Y%m') = @MONTH
