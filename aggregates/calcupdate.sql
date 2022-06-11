8c39754ac605d1431f9a2f5f317cc0b9cf4c7d4e
= <=>
2022-06-11 10:28:29 +0200

    Agrégation des données des avis pour remplacer tous les usages qui calculent aujourd'hui ces données à partir des avis détaillés #1129

--- aggregates/calcupdate.sql
+++ aggregates/calcupdate.sql
@@ -1,27 +1,3 @@
-delete from avis_byday where date_format(day,'%Y%m') = @MONTH;
-insert ignore into avis_byday 
-select cast(xwd_creation_date as date) as day, xws_value as demarche, count(*) as count, 
-sum(CASE WHEN score.xwi_value=3 THEN 1 ELSE 0 END) as avis3, 
-sum(CASE WHEN score.xwi_value=2 THEN 1 ELSE 0 END) as avis2, 
-sum(CASE WHEN score.xwi_value=1 THEN 1 ELSE 0 END) as avis1, 
-sum(CASE WHEN facile.xwi_value=3 THEN 1 ELSE 0 END) as facile3, 
-sum(CASE WHEN facile.xwi_value=2 THEN 1 ELSE 0 END) as facile2, 
-sum(CASE WHEN facile.xwi_value=1 THEN 1 ELSE 0 END) as facile1, 
-sum(CASE WHEN compr.xwi_value=3 THEN 1 ELSE 0 END) as compr3, 
-sum(CASE WHEN compr.xwi_value=2 THEN 1 ELSE 0 END) as compr2, 
-sum(CASE WHEN compr.xwi_value=1 THEN 1 ELSE 0 END) as compr1, 
-null, null, null, null, null, null, null, 
-null, null, null, null, null, null, 
-null, null, null, 
-null 
-from xwikidoc use index (doc_creation_date), xwikistrings demarche, 
-xwikiobjects avis left outer join xwikiintegers score on (xwo_id=score.xwi_id and score.xwi_name='score' )
-left outer join xwikiintegers facile on (xwo_id=facile.xwi_id and facile.xwi_name='facile')
-left outer join xwikiintegers compr on (xwo_id=compr.xwi_id and compr.xwi_name='comprehensible')
-where xwd_fullname=xwo_name and xwo_classname='Avis.Code.AvisClass' 
-and xwo_id=xws_id and xws_name='demarche' 
-and date_format(xwd_creation_date,'%Y%m') = @MONTH
-group by 1,2;
 update avis_byday agg JOIN (
 select cast(xwd_creation_date as date) as day, xws_value as demarche,
 sum(CASE WHEN diff.xwl_value='manque-d-informations' THEN 1 ELSE 0 END) as diff_manquedinformations,
---
+++ aggregates/calcupdatebymonth.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+YEAR=$1
+for MONTH in 01 02 03 04 05 06 07 08 09 10 11 12; do
+echo $MONTH;
+mysql -u root xwiki -e "set @MONTH=$YEAR$MONTH; source $(dirname "$0")/calcupdate.sql;"
+done
